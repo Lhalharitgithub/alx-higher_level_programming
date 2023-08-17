72a30e8bef1c4407af4699f795fd855540413086
Lhalharitgithub <aziwemputa@gmail.com>
2023-08-16 18:23:36 -0700

    Python - More Data Structures: Set, Dictionary ALL_UPDATED

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-#0-square_matrix_simple.py
-
-
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i in(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
     return (new_matrix)
