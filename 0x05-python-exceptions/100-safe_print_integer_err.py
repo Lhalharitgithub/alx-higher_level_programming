25ff4d368200647929efd1ad5d6a5fd8f82ec8d0
Lhalharitgithub <aziwemputa@gmail.com>
2023-08-28 17:57:15 -0700

    Python - Exceptions ALL_UP_TO_DATE

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 
 
 def safe_print_integer_err(value):
-    """It Writez a function that printz an integer."""
+    """function that printz an integer."""
     
     try:
         print("{:d}".format(value))
