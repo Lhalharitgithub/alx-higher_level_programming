8525934a2b10a1656a385f41e78078b540ca3e5b
Lhalharitgithub <aziwemputa@gmail.com>
2023-08-28 17:53:49 -0700

    Python - Exceptions ALL_UP_TO_DATE

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """This is a function that prints x elements of a list."""
-    
-    num = 0
+    """function that prints x elements of a list."""
+     num = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
